1edf8a12259f493db146eab6e6bcd41f8dbce045
Alexandr Nikitin <nikitin.alexandr.a@gmail.com>
2018-03-16 10:48:36 +0200

    Add importance

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -24,8 +24,8 @@ def main(input_filepath, output_filepath):
 def construct_line(row):
     label = str(2 * int(row['is_attributed']) - 1)
     str_vw = f"{label}"
-    #importance = '100' if label == 1 else ''
-    #str_vw = str_vw.join(importance)
+    importance = '$importance$' if label == 1 else ''
+    str_vw += f" {importance} "
     str_vw += f" |i {row['ip']}"
     str_vw += f" |a {row['app']}"
     str_vw += f" |d {row['device']}"
