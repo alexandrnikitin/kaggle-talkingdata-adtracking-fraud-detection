71f54e1dd0f340a3a595d0f8459d9a688bc5b8ed
Alexandr Nikitin <nikitin.alexandr.a@gmail.com>
2018-03-28 13:04:16 +0300

    csv2vw

--- src/data/csv2vw.py
+++ src/data/csv2vw.py
@@ -2,12 +2,31 @@
 
 
 def construct_line(row):
+    ignore = []
+    categorical = ['ip', 'app', 'device', 'os', 'channel']
+    numerical = []
+
     label = 2 * int(row['is_attributed']) - 1
+
     str_vw = f"{label}"
-    str_vw += f" |i {row['ip']}"
-    str_vw += f" |a {row['app']}"
-    str_vw += f" |d {row['device']}"
-    str_vw += f" |o {row['os']}"
-    str_vw += f" |c {row['channel']}"
+    for k, v in row.items():
+        print(k)
+        if k in ignore:
+            continue
+
+        if k in categorical:
+            str_vw += f" |{k} {v}"
+        elif k in numerical:
+            str_vw += f" |{k} {k}:{v}"
+        else:
+            categorical_prefixes = ['MODE','DAY','YEAR','MONTH','WEEKDAY']
+            numerical_prefixes = ['SUM','STD','MAX','SKEW','MIN','MEAN','COUNT','NUM_UNIQUE']
+            if k.startswith(tuple(categorical_prefixes)):
+                str_vw += f" |{k} {v}"
+            elif k.startswith(tuple(numerical_prefixes)):
+                str_vw += f" |{k} {k}:{v}"
+            else:
+                str_vw += f" |{k} {v}"
+
     str_vw += '\n'
     return str_vw
